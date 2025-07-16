8fa5a02b08ba17af5006a2823661f2c0826b1c84
bane.shi <fengxiaotx@163.com>
2025-07-16 20:16:21 +0800

    feat:

--- triones-alipay-spring-boot-starter/src/main/java/com/trionesdev/payment/alipay/spring/boot/autoconfigure/AlipayAutoConfiguration.java
+++ triones-alipay-spring-boot-starter/src/main/java/com/trionesdev/payment/alipay/spring/boot/autoconfigure/AlipayAutoConfiguration.java
@@ -1,9 +1,12 @@
 package com.trionesdev.payment.alipay.spring.boot.autoconfigure;
 
+import com.trionesdev.payment.alipay.v2.Alipay;
+import com.trionesdev.payment.alipay.v2.AlipayIntegrationConfig;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
@@ -13,4 +16,18 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties(value = {AlipayPayProperties.class})
 public class AlipayAutoConfiguration {
     private final AlipayPayProperties alipayPayProperties;
+
+    @Bean
+    public Alipay alipay(){
+        return new Alipay(alipayIntegrationConfig());
+    }
+
+    private AlipayIntegrationConfig alipayIntegrationConfig(){
+        AlipayIntegrationConfig config = new AlipayIntegrationConfig();
+        config.setAppId(alipayPayProperties.getAppId());
+        config.setPrivateKey(alipayPayProperties.getPrivateKey());
+        config.setAlipayPublicKey(alipayPayProperties.getAlipayPublicKey());
+
+        return config;
+    }
 }
--- triones-alipay-spring-boot-starter/src/main/java/com/trionesdev/payment/alipay/spring/boot/autoconfigure/AlipayPayProperties.java
+++ triones-alipay-spring-boot-starter/src/main/java/com/trionesdev/payment/alipay/spring/boot/autoconfigure/AlipayPayProperties.java
@@ -3,8 +3,26 @@ package com.trionesdev.payment.alipay.spring.boot.autoconfigure;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.Map;
+
 @ConfigurationProperties(prefix = "triones.payment.alipay")
 @Data
 public class AlipayPayProperties {
     private Boolean enabled;
+    private String appId;
+    private String privateKey;
+    private String alipayPublicKey;
+    private String appCertPath;
+    private String alipayPublicCertPath;
+    private String rootCertPath;
+    private String appCertContent;
+    private String alipayPublicCertContent;
+    private String rootCertContent;
+
+    private String transactionNotifyUrl;
+    private String refundNotifyUrl;
+    private String transferNotifyUrl;
+    private Map<String, String> transactionNotifyUrls;
+    private Map<String, String> refundNotifyUrls;
+    private Map<String, String> transferNotifyUrls;
 }
